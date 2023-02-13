965e0b808585338fdddec57efccc3ae99553c7cc
martin <martinbruun1995@gmail.com>
2023-02-13 10:41:55 +0100

    Feat: Final commit

--- navbarAnimation.js
+++ navbarAnimation.js
@@ -3,6 +3,9 @@
   navbar.forEach((link) => {
     // ON mouse over navbar link
     link.addEventListener('mouseover', () => {
+      if (link.textContent.includes('HOME')) {
+        return;
+      }
       link.classList.add('border');
     });
     // On mouse out of navbar link
